ac0cc5c7d8227ca1c82dd9c76950eb90e8a2e49a
Tom Trapman <t.h.trapman@bath.ac.uk>
2019-01-16 12:23:09 +0000

    Commit four

--- GitHub_Trial.R
+++ GitHub_Trial.R
@@ -17,3 +17,14 @@ read.csv("GardenOzone.csv")
 knsklgmkmrs
 
 GardenOzone.csv
+mean(Ozone)
+Concentration <- read.csv("GardenOzone.csv")
+mean(Ozone)
+Concentration
+mean("Ozone")
+Garden.location
+plot(Concentration)
+mean(Concentration)
+median(Concentration)
+var(Concentration)
+summary(Concentration)
